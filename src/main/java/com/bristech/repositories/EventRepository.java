1ba5e115aa2c6ee0ed5159881177d2065b18acec
TheWalkingFridge <martin.dimi97@gmail.com>
2018-03-11 16:01:06 +0000

    version 1

--- src/main/java/com/bristech/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bristech.repositories;
-
-
-import com.bristech.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long>{
-
-}
